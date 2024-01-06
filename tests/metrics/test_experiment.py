c370513ff5955dca416808f0277c6faac2fe0c11
Kairo Silveira <kairo.silveira.contato@gmail.com>
2024-01-06 14:19:33 -0300

    metrics - test implemented for kkstrategy method

--- tests/metrics/test_experiment.py
+++ tests/metrics/test_experiment.py
@@ -6,7 +6,8 @@ from hyperopt import hp
 
 
 def test_get_experiment_interval(sample_historical_data):
-    exp = Experiment(sample_historical_data, skip_days=2, train_days=2,retrain_freq=2)
+    exp = Experiment(sample_historical_data, skip_days=2,
+                     train_days=2, retrain_freq=2)
     start_date, end_date = exp._get_experiment_interval()
 
     assert isinstance(start_date, datetime)
@@ -15,85 +16,34 @@ def test_get_experiment_interval(sample_historical_data):
     assert end_date == dt.strptime('2023-01-04', '%Y-%m-%d')
 
 
-# def test__get_train_test_dates(sample_historical_data):
-#     exp1 = Experiment(sample_historical_data, skip_days=2, retrain_freq=2)
-#     exp2 = Experiment(sample_historical_data, skip_days=2, retrain_freq=3)
-
-#     result1 = exp1._get_train_test_dates()
-#     result2 = exp2._get_train_test_dates()
-
-#     result1_first_expexted = (
-#         dt.strptime('2022-12-27', '%Y-%m-%d'),
-#         dt.strptime('2022-12-28', '%Y-%m-%d'))
-#     result1_last_expexted = (
-#         dt.strptime('2023-01-02', '%Y-%m-%d'),
-#         dt.strptime('2023-01-03', '%Y-%m-%d'))
-#     result2_first_expexted = (
-#         dt.strptime('2022-12-27', '%Y-%m-%d'),
-#         dt.strptime('2022-12-29', '%Y-%m-%d'))
-#     result2_last_expexted = (
-#         dt.strptime('2023-01-02', '%Y-%m-%d'),
-#         dt.strptime('2023-01-04', '%Y-%m-%d'))
-
-#     assert isinstance(result1, list)
-#     assert len(result1) == 4
-#     assert len(result2) == 3
-#     assert result1[0] == result1_first_expexted
-#     assert result1[-1] == result1_last_expexted
-#     assert result2[0] == result2_first_expexted
-#     assert result2[-1] == result2_last_expexted
+def date(date_str1, date_str2):
+    return dt.strptime(date_str1, '%Y-%m-%d'), dt.strptime(date_str2, '%Y-%m-%d')
 
 
-# def test_kk_strategy(sample_historical_data, sample_parameters, sample_kkmultiple):
-#     exp1 = Experiment(sample_historical_data, skip_days=2, retrain_freq=2)
-#     exp2 = Experiment(sample_historical_data, skip_days=2, retrain_freq=3)
+def test__get_train_test_dict(sample_historical_data):
+    exp = Experiment(sample_historical_data, skip_days=2,
+                     train_days=2, retrain_freq=2)
+    train_test_dict = exp._get_train_test_dict()
 
-#     # calculate expected values for retrain_freq = 2
+    expected = {
+        date('2022-12-25', '2022-12-26'): date('2022-12-27', '2022-12-28'),
+        date('2022-12-27', '2022-12-28'): date('2022-12-29', '2022-12-30'),
+        date('2022-12-29', '2022-12-30'): date('2022-12-31', '2023-01-01'),
+        date('2022-12-31', '2023-01-01'): date('2023-01-02', '2023-01-03')
+    }
 
-#     expected_accumulated1 = 0
-#     accumulator = CryptoAccumulator(
-#         sample_historical_data, ('2022-12-29', '2022-12-30'), kkmult=sample_kkmultiple)
-#     result = accumulator.get_accumulated_value()
-#     expected_accumulated1 += result.amount_accumulated
+    assert train_test_dict == expected
 
-#     accumulator = CryptoAccumulator(
-#         sample_historical_data, ('2022-12-31', '2023-01-01'), kkmult=sample_kkmultiple)
-#     result = accumulator.get_accumulated_value(
-#         remaining_budget=result.remaining_budget)
-#     expected_accumulated1 += result.amount_accumulated
 
-#     accumulator = CryptoAccumulator(
-#         sample_historical_data, ('2023-01-02', '2023-01-03'), kkmult=sample_kkmultiple)
-#     result = accumulator.get_accumulated_value(
-#         remaining_budget=result.remaining_budget)
+def test_kk_strategy(sample_historical_data, sample_kk_parameters):
 
-#     # expected values
-#     expected_accumulated1 += result.amount_accumulated
-#     expected_remaining_budget1 = result.remaining_budget
+    exp = Experiment(sample_historical_data, retrain_freq=3,
+                     train_days=3, skip_days=2)
 
-#     # calculate expected values for retrain_freq = 3
-#     expected_accumulated2 = 0
-#     accumulator = CryptoAccumulator(
-#         sample_historical_data, ('2022-12-30', '2023-01-01'), kkmult=sample_kkmultiple)
-#     result = accumulator.get_accumulated_value()
-#     expected_accumulated2 += result.amount_accumulated
+    expected_accumulated = (1000/100)*200
+    with patch('metrics.experiment.train') as mock_train:
+        mock_train.return_value = sample_kk_parameters
 
-#     accumulator = CryptoAccumulator(
-#         sample_historical_data, ('2023-01-02', '2023-01-04'), kkmult=sample_kkmultiple)
-#     result = accumulator.get_accumulated_value(
-#         remaining_budget=result.remaining_budget)
+        result = exp.kkmultiple_strategy(space_params={})
 
-#     # expected values
-#     expected_accumulated2 += result.amount_accumulated
-#     expected_remaining_budget2 = result.remaining_budget
-
-#     with patch('metrics.experiment.train') as mock_train:
-#         mock_train.return_value = sample_parameters
-
-#         result1 = exp1.kk_strategy(space_params={})
-#         result2 = exp2.kk_strategy(space_params={})
-
-#     assert result1.amount_accumulated == expected_accumulated1
-#     assert result1.remaining_budget == expected_remaining_budget1
-#     assert result2.amount_accumulated == expected_accumulated2
-#     assert result2.remaining_budget == expected_remaining_budget2
+    assert result == expected_accumulated
